fa2c75b178591664ef23ca2b686c6722ddc0b8dc
Michael Mocan <michael@knd.com.au>
2011-04-05 12:09:31 +1000

    changed index to head

---
+++ head.php
@@ -0,0 +1,16 @@
+?php
+
+	echo "HELLO WORLD";
+	echo " this is a git example commit" 
+	echo "removed all those Ds";
+?>
+
+>>>>>>>>>>>>>>>>>>>
+
+ some mediocre content
+  
+ this is some text to try and cause a conflict 
+
+ some mediocre content
+  
+>>>>>>>>>>>>>>>>>>
